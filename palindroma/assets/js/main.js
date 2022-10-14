f630950fd68075f64b1a5c53058af8e3b44d732b
MaTtiaa01 <mattia2001@live.it>
2022-10-14 12:33:39 +0200

    add reverse function

--- palindroma/assets/js/main.js
+++ palindroma/assets/js/main.js
@@ -1,10 +1,24 @@
 // Chiedere all’utente di inserire una parola 
 // mi creo una costante con all'interno un prompt
 
+const userWord = prompt("Insert a word")
+console.log(userWord);
 
 
 //Creare una funzione per capire se la parola inserita è palindroma
 //prendo la parola e uso .reverseString
 //comparo la parola del prompt con quella nel .reverseString
 //se sono uguali sono plaindrome altrimenti no
+function reverseWords(str) {
+    let reverseWordArr = str.split(" ").map(word => word.split("").reverse().join(""));
+    return reverseWordArr.join(" ");
+}
+
+const reversedWord = reverseWords(userWord);
+console.log(reversedWord);
+
+function plaindromeCheck() {
+    
+   
+}
 
