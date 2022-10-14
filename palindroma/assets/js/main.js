e303d9d5515127adec428fc50723466d1a1ae6c9
MaTtiaa01 <mattia2001@live.it>
2022-10-14 12:16:36 +0200

    add comments

--- palindroma/assets/js/main.js
+++ palindroma/assets/js/main.js
@@ -1,1 +1,10 @@
-alert("ti works");
+// Chiedere all’utente di inserire una parola 
+// mi creo una costante con all'interno un prompt
+
+
+
+//Creare una funzione per capire se la parola inserita è palindroma
+//prendo la parola e uso .reverseString
+//comparo la parola del prompt con quella nel .reverseString
+//se sono uguali sono plaindrome altrimenti no
+
